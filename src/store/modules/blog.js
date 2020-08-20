b9df5514180d5341f992b38cf9ed428a19988fc0
Nguyễn Minh Trí <17521156@ms.uit.edu.vn>
2020-08-20 10:47:26 +0700

    fix edit blog

--- src/store/modules/blog.js
+++ src/store/modules/blog.js
@@ -3,14 +3,16 @@ import router from "./../../router";
 const state = {
   loading: false,
   data: null,
-  err: null
+  err: null,
+  oneBlog: null,
 };
 
 const mutations = {
   storeBlogRequest(state) {
     state.loading = true;
     state.data = null,
-      state.err = null
+    state.err = null,
+    state.oneBlog =  null
   },
 
   storeBlogSuccess(state, payload) {
@@ -25,21 +27,30 @@ const mutations = {
     state.err = payload
   },
 
-  storeUpdateBlog(state, blog){
+  storeUpdateBlog(state, blog) {
     if (blog) {
       state.data = state.data.map(item => {
-        if (item._id === blog._id){
+        if (item._id === blog._id) {
           item = blog
         }
         return item
       })
     }
+  },
+
+  storeSetOneBlog(state, blog) {
+    state.oneBlog = blog
+  },
+
+  storeGetOneBlog(state) {
+    return state.oneBlog
   }
+  
 };
 
 const actions = {
   fetchListBlogs({ commit }) {
-    commit("storeBlogRequest");
+    //commit("storeBlogRequest");
     api.get("/blogs/")
       .then((result) => {
         commit("storeBlogSuccess", result.data);
@@ -53,7 +64,7 @@ const actions = {
     commit("storeBlogRequest");
     api.get(`/blogs/${id}`)
       .then(result => {
-        commit("storeBlogSuccess", result.data);
+        commit("storeSetOneBlog", result.data);
       })
       .catch(err => {
         commit("storeBlogFailed", err);
@@ -81,14 +92,14 @@ const actions = {
   postBlog({ commit }, blog) {
     commit("storeBlogRequest");
     api
-    .post("/blogs",blog)
-    .then(result=>{
-      commit("storeBlogSuccess", result.data);
-      router.replace("/admin/blogs/add");
-    })
-    .catch(err => {
-      commit("storeBlogFailed", err);
-    })
+      .post("/blogs", blog)
+      .then(result => {
+        commit("storeBlogSuccess", result.data);
+        router.replace("/admin/blogs/add");
+      })
+      .catch(err => {
+        commit("storeBlogFailed", err);
+      })
   }
 }
 
--- src/views/adminTemplate/blogPage/edit.vue
+++ src/views/adminTemplate/blogPage/edit.vue
@@ -1,58 +1,87 @@
 <template>
-    <div class="container-fluid">
-        <h1 class="h4 mb-2 text-gray-800">Cập Nhập Bài Viết</h1>
-        <form @submit.prevent="handleSubmit" role="form" method="POST" enctype="multipart/form-data">
-            <div class="row">
-                <div class="col-sm-8">
-                    <div class="card shadow mb-4">
-                        <div class="card-body">
-                            <div class="form-group ">
-                                <label for="exampleInputEmail1">Tên bài viết</label>
-                                <input v-model="name" type="text" class="form-control" name="name" autocomplete="off">
-                            </div>
-                            <div class="form-group ">
-                                <label for="exampleInputEmail1">Mô tả</label>
-                                <textarea v-model="description" name="description" class="form-control" cols="5" rows="2" autocomplete="off"></textarea>
-                            </div>
-                            <div class="form-group ">
-                                <label for="exampleInputEmail1">Nội dung</label>
-                                <ckeditor name="content" :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
-                            </div>
-                            
-                        </div>
-                    </div>
-                    
-                </div>
-                <div class="col-sm-4">
-                <div class="card shadow mb-4">
-                        <div class="card-body">
-                            <div class="form-group ">
-                                <label for="exampleInputEmail1">Tiêu đề SEO</label>
-                                <input v-model="titleSeo" type="text" class="form-control" name="titleSeo" autocomplete="off">
-                            </div>
-                            <div class="form-group ">
-                                <label for="exampleInputEmail1">Mô tả SEO</label>
-                                <textarea v-model="descriptionSeo" name="descriptionSeo" class="form-control" cols="5" rows="2" autocomplete="off"></textarea>
-                            </div>
-                            <div class="form-group ">
-                                <label for="exampleInputEmail1">Từ khóa SEO</label>
-                                <textarea v-model="keywordSeo" name="keywordSeo" class="form-control" cols="5" rows="2" autocomplete="off"></textarea>
-                                <p>{{blog.keywordSeo}}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 ">
-                    <div class="card shadow mb-4">
-                        <div class="card-footer text-center">
-                            <button type="submit" class="btn btn-warning"><i class="fa fa-save"></i> Hủy bỏ </button>&nbsp;
-                            <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Thêm mới </button>
-                        </div>
-                    </div>
-                </div>
-            </div> 
-        </form>
-    </div>
+  <div class="container-fluid">
+    <h1 class="h4 mb-2 text-gray-800">Cập Nhập Bài Viết</h1>
+    <form @submit.prevent="handleSubmit" role="form" method="POST" enctype="multipart/form-data">
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="card shadow mb-4">
+            <div class="card-body">
+              <div class="form-group">
+                <label for="exampleInputEmail1">Tên bài viết</label>
+                <input
+                  v-model="name"
+                  type="text"
+                  class="form-control"
+                  name="name"
+                  autocomplete="off"
+                />
+              </div>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Mô tả</label>
+                <textarea
+                  v-model="description"
+                  name="description"
+                  class="form-control"
+                  cols="5"
+                  rows="2"
+                  autocomplete="off"
+                ></textarea>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Nội dung</label>
+                <ckeditor
+                  name="content"
+                  :editor="editor"
+                  v-model="editorData"
+                  :config="editorConfig"
+                ></ckeditor>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card shadow mb-4">
+            <div class="card-body">
+              <div class="form-group">
+                <label for="exampleInputEmail1">Tiêu đề SEO</label>
+                <input
+                  v-model="titleSeo"
+                  type="text"
+                  class="form-control"
+                  name="titleSeo"
+                  autocomplete="off"
+                />
+              </div>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Mô tả SEO</label>
+                <textarea
+                  v-model="descriptionSeo"
+                  name="descriptionSeo" class="form-control" cols="5" rows="2"  autocomplete="off"></textarea>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Từ khóa SEO</label>
+                <textarea
+                  v-model="keywordSeo"
+                  name="keywordSeo" class="form-control"  cols="5" rows="2" autocomplete="off"></textarea>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12">
+          <div class="card shadow mb-4">
+            <div class="card-footer text-center">
+              <button type="submit" class="btn btn-warning">
+                <i class="fa fa-save"></i> Hủy bỏ
+              </button>&nbsp;
+              <button type="submit" class="btn btn-primary">
+                <i class="fa fa-save"></i> Thêm mới
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -68,27 +97,35 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
                 editorConfig: {
                     // The configuration of the editor.
                 },
-                name:this.blog.name,//ví dụ nè
-                description:'',
+                name: "",//ví dụ nè
+                description: "",
                // content:"",
-                titleSeo:'',
-                descriptionSeo:'',
-                keywordSeo:''
+                titleSeo: "",
+                descriptionSeo: "",
+                keywordSeo: ""
             };
         },
-        created() {
-          this.$store.dispatch("fetchDetailBlog", this.$route.params.id);// cái này phải làm sao mà có dữ liệu rồi mới bỏ vào data
-          console.log(this.$store.state.blog.data)// push code lên đi đê xem thử
+        beforeCreate () {
+            this.$store.dispatch("fetchDetailBlog", this.$route.params.id);
+        },
+        watch : {
+          blog () {
+            this.name = this.blog.name;
+            this.description = this.blog.description;
+            this.titleSeo = this.blog.titleSeo;
+            this.descriptionSeo = this.blog.descriptionSeo;
+            this.keywordSeo = this.blog.keywordSeo;
+            this.editorData = this.blog.content
+          }
         },
 
         computed:{
           blog(){// blog này là sao vậy //tương tự như ông thầy đí
-            return this.$store.state.blog.data
+            return this.$store.state.blog.oneBlog
           }
-        }
+        },
     }
 </script>
 
 <style>
-
 </style>
