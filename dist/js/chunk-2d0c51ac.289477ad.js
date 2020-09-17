b7e48debbe0d561dc24272b977c84920c22332da
Nguyễn Minh Trí <17521156@ms.uit.edu.vn>
2020-09-17 15:36:29 +0700

    make it better

---
+++ dist/js/chunk-2d0c51ac.289477ad.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c51ac"],{"3e59":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v("about page")])},u=[],a={},l=a,o=t("2877"),r=Object(o["a"])(l,c,u,!1,null,null,null);n["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d0c51ac.289477ad.js.map
