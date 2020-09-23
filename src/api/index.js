0e7cffcaaa7f66d5357d990fe49435201a67a845
Nguyễn Minh Trí <17521156@ms.uit.edu.vn>
2020-09-23 15:58:55 +0700

    fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL : "http://localhost:5000/api"
+  baseURL : "https://vexeonline-api.herokuapp.com/api"
 })
 //https://vexeonline-api.herokuapp.com/api
+//http://localhost:5000/api
